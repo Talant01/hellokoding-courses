b50b405e84c4b5d9b772389a6e6bf3462b8892bc
Talant <talantjenishbekuulu@gmail.com>
2019-08-17 11:23:12 +0600

    Order form is created

--- springboot-examples/springboot-thymeleaf-helloworld/src/main/java/tacos/web/DesignTacoController.java
+++ springboot-examples/springboot-thymeleaf-helloworld/src/main/java/tacos/web/DesignTacoController.java
@@ -3,7 +3,9 @@ package tacos.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacos.Ingredient;
 
@@ -13,6 +15,8 @@ import java.util.stream.Collectors;
 
 import tacos.Ingredient.Type;
 import tacos.Taco;
+
+import javax.validation.Valid;
 
 
 @Slf4j
@@ -41,6 +45,13 @@ public class DesignTacoController{
         }
         model.addAttribute("design",new Taco());
         return "design";
+    }
+    @PostMapping
+    public String processDesign(@Valid Taco design, Errors errors) {
+        if (errors.hasErrors()) return "design";
+        log.info("Processing design: " + design);
+
+        return "redirect:/orders/current";
     }
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
         return ingredients
